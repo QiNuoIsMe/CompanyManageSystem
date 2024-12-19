d9f8a64dabac325713f902be9fffd71859adf766
qinuo0824 <2017256701@qq.com>
2024-12-19 21:43:06 +0800

    添加子部门表单结构

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -2,6 +2,29 @@
     <!-- 组件之间可以通过属性（props）和事件（events）进行通信。展示了如何从子组件向父组件传递信息，特别是用于更新父组件中的数据状态。这种模式被称为“父子组件通信”。 -->
     <el-dialog title="新增部门" :visible="showDialog" @close="close">
         <!-- 放置弹层内容 -->
+        <el-form label-width="120px">
+            <el-form-item label="部门名称"><!--placeholder背景内容-->
+                <el-input placeholder="2-10个字符" size="mini" style="width:80%"></el-input>
+            </el-form-item>
+            <el-form-item label="部门编码">
+                <el-input placeholder="2-10个字符" size="mini" style="width:80%"></el-input>
+            </el-form-item>
+            <el-form-item label="部门负责人">
+                <el-select  placeholder="请选择部门负责人" size="mini" style="width:80%" ></el-select>
+            </el-form-item>
+            <el-form-item label="部门介绍">
+                <el-input type="textarea" :rows="4" placeholder="1-100个字符" size="mini" style="width:80%"></el-input>
+            </el-form-item>
+            <!-- 按钮 -->
+            <el-form-item>
+                <el-row type="flex" justify="center"><!--水平居中-->
+                    <el-col :span="12">
+                        <el-button type="primary" size="mini">确定</el-button>
+                        <el-button size="mini">取消</el-button>
+                    </el-col>
+                </el-row>
+            </el-form-item>
+        </el-form>
     </el-dialog>
 </template>
 
