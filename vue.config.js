58485b0b272b0b11defdbb2abe84c2edb394634a
qinuo0824 <2017256701@qq.com>
2024-12-25 03:02:15 +0800

    记录树的节点

--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,7 @@ module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
-    // 关闭语法检查
+    // 关闭语法检查---------
   lintOnSave: false,
   // lintOnSave: process.env.NODE_ENV === 'development',
   productionSourceMap: false,
